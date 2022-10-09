88cd79d66bf7ca6b77cea6a1e48a0ebe6506bbe1
Jayant Rohila <jrohila55@gmail.com>
2022-10-09 02:56:52 -0700

    update

--- docs/Main.java
+++ docs/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     static void Pattern(int n) {
         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  ");
+            }
             for (int j = i; j <= n; j++) {
                 System.out.print("* ");
             }
